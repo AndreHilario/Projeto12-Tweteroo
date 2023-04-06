ba5f24e72b54a959201271737e461459fa9666c3
Andre Hilario <andre18hilario@gmail.com>
2023-04-06 08:37:50 -0300

    Post /sign-up working

--- src/app.js
+++ src/app.js
@@ -5,5 +5,25 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const newUsersServer = [];
+const newTweetsServer = [];
+
+app.post("/sign-up", (req, res) => {
+
+    const { username, avatar } = req.body;
+    const newUser = { username, avatar };
+
+    newUsersServer.push(newUser);
+    res.send("OK");
+})
+
+app.post("tweets", (req, res) => {
+
+})
+
+app.get("tweets", (req, res) => {
+
+})
+
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Tweets funcionando na porta ${PORT}`));
